91ef004df0de71ab7ad41f9d3539cd25c811f03d
PiotrWojniak <wojospajder@yahoo.ie>
2021-09-08 12:31:29 +0000

    Added product sorting

--- products/views.py
+++ products/views.py
@@ -10,9 +10,10 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
-
         """ filtering by categories """
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -29,10 +30,29 @@ def all_products(request):
                 queries = Q(name__icontains=query) | Q(description__icontains=query)
                 products = products.filter(queries)
 
+        """ sorting """
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'sorting': sorting,
     }
 
     return render(request, 'products/products.html', context)
