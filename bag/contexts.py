f6da48f0979eff856eb8a9456fee937379c2da0d
PiotrWojniak <wojospajder@yahoo.ie>
2021-09-18 18:02:31 +0000

    Finish product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,16 +11,30 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    # Access bag from session and calculating total cost
+    # Access bag from session, calculating cost and check product size
     for item_id, item_data in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += item_data * product.price
-        product_count += item_data
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': item_data,
-            'product': product,
-        })
+        # product without size
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        # product with size
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     # Calculating free shipping
     if total < settings.FREE_SHIPPING_THRESHOLD:
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def view_bag(request):
@@ -12,13 +13,26 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})  # keeping shopping in user session
 
-    # Add or update quantity in the bag
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        # add or update product with size
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        # add or update product without size
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
