fc650fbb302d5fd1beac99a61ce4bcfa0e11f9ec
PiotrWojniak <wojospajder@yahoo.ie>
2021-09-25 12:46:56 +0000

    Added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JBgEgKtcGNIskVi6Ev8w0GAmZ6JNxxUcAs6Ed7yTHBhDlkJioWaelYcv0CmYUHHNOZyQo4YZGy48DiV06x5CEYN00DVBapr2Y',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
