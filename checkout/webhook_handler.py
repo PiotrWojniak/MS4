1401635a948350b1c5f84cbb9add81789b9b467e
PiotrWojniak <wojospajder@yahoo.ie>
2021-09-26 12:51:05 +0000

    Added webhook hendler

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+
+
+# To prevent confirm the payment when user close the page before form is submitted
+class StripeWH_Handler:
+    """Handle Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
