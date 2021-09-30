71b9391e4ce5ce9154ff704013e1ee2f4a66c950
PiotrWojniak <wojospajder@yahoo.ie>
2021-09-30 16:30:59 +0000

    Update WH-hadler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -41,6 +42,21 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked added early
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_county = shipping_details.address.state
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_country = shipping_details.address.country
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -52,8 +68,8 @@ class StripeWH_Handler:
                     street_address1__iexact=shipping_details.address.line1,
                     street_address2__iexact=shipping_details.address.line2,
                     town_or_city__iexact=shipping_details.address.city,
-                    postcode__iexact=shipping_details.address.postal_code,
                     county__iexact=shipping_details.address.state,
+                    postcode__iexact=shipping_details.address.postal_code,
                     country__iexact=shipping_details.address.country,
                     grand_total=grand_total,
                     original_bag=bag,
@@ -73,13 +89,14 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     street_address1=shipping_details.address.line1,
                     street_address2=shipping_details.address.line2,
                     town_or_city=shipping_details.address.city,
-                    postcode=shipping_details.address.postal_code,
                     county=shipping_details.address.state,
+                    postcode=shipping_details.address.postal_code,
                     country=shipping_details.address.country,
                     original_bag=bag,
                     stripe_pid=pid
