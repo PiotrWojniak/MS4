81db2e30cb1ec38a008ead57d21907b158e27b6f
PiotrWojniak <wojospajder@yahoo.ie>
2021-10-01 12:32:08 +0000

    Add add_product template, view and url

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import Product, Category
-from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
@@ -8,8 +7,6 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
