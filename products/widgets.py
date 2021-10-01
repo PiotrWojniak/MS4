6dda2d39a98afcae91fbb8d6b0f3eec1e01976df
PiotrWojniak <wojospajder@yahoo.ie>
2021-10-01 16:54:41 +0000

    Add widget to fixing the image field

---
+++ products/widgets.py
@@ -0,0 +1,11 @@
+from django.forms.widgets import ClearableFileInput
+# Using "as _" means we can now call gettext_lazy() using _().
+# It's effectively an alias.
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomClearableFileInput(ClearableFileInput):
+    clear_checkbox_label = _('Remove')
+    initial_text = _('Current Image')
+    input_text = _('')
+    template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
