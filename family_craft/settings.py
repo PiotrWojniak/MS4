82c9a45aad9b9cdc3f38b511051bc2b3bebe62d3
PiotrWojniak <wojospajder@yahoo.ie>
2021-10-02 18:37:58 +0000

    Removed secret key and set debug

--- family_craft/settings.py
+++ family_craft/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tlyln#bt@5&%w-twmo)&k)z6guqp@0vmrm^o&patrm6f7*)r*j'
+SECRET_KEY = = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['piotr-wojniak-fc-ms4.herokuapp.com', 'localhost']
 
