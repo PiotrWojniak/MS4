6c7026c35843858bce6e3fe0d74c3f5bd8edae7a
PiotrWojniak <wojospajder@yahoo.ie>
2021-10-28 06:32:28 +0000

    Fix Auth_password_validation and update file for readme

--- family_craft/settings.py
+++ family_craft/settings.py
@@ -34,13 +34,13 @@ ALLOWED_HOSTS = ['piotr-wojniak-fc-ms4.herokuapp.com', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django.contrib.admin',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -145,20 +145,16 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.\
-            UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation\
-            .MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation\
-            .CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation\
-            .NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
